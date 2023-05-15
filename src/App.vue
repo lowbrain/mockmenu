3856e94891f44f0c3d2bd2f697b497409a1319e7
lowbrain <83059975+lowbrain@users.noreply.github.com>
2023-05-15 22:01:15 +0900

    アプリ設定値をうまい具合に修正

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+console.log(`import.meta.env.MODE:${import.meta.env.MODE}`)
+console.log(`import.meta.env.BASE_URL:${import.meta.env.BASE_URL}`)
+console.log(`import.meta.env.PROD:${import.meta.env.PROD}`)
+console.log(`import.meta.env.DEV:${import.meta.env.DEV}`)
+console.log(`import.meta.env.VITE_APP_TITLE:${import.meta.env.VITE_APP_TITLE}`)
+console.log(`import.meta.env.VITE_APP_VERSION:${import.meta.env.VITE_APP_VERSION}`)
+console.log(`import.meta.env.VITE_APP_BUILDTS:${import.meta.env.VITE_APP_BUILDTS}`)
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import vuetify from 'vite-plugin-vuetify'
@@ -11,15 +11,26 @@ const pwacnf = {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA(pwacnf),
-    vuetify()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({ command, mode }) => {
+  process.env.VITE_APP_VERSION = require("./package.json").version
+  process.env.VITE_APP_BUILDTS = (new Date()).toLocaleString()
+  const env = loadEnv(mode, process.cwd(), '')
+  const devtoolFlg = mode.indexOf("development") === 0 ? true : false
+  return {
+    base: env.APP_PATH,
+    plugins: [
+      vue(),
+      VitePWA(pwacnf),
+      vuetify()
+    ],
+    define: {
+      __VUE_PROD_DEVTOOLS__: devtoolFlg
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
